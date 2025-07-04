cc29aa64c3e81d7fd529a1796aa5f190042109ea
ayush20152015 <atyud2015@gmail.com>
2025-07-04 12:40:00 +0530

    updated

---
+++ Model/PatternDatabase/PatternDatabase.h
@@ -0,0 +1,19 @@
+//
+// Created by toroid_5 on 01-06-2025.
+//
+
+#ifndef RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
+#define RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
+
+template<typename T>
+class PatternDatabaseEstimate {
+private:
+
+public:
+    int getEstimate(T rubiksCube) {
+        return 0;
+    }
+
+};
+
+#endif //RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
